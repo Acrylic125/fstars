b18313d1a4ac0a3acc5b4831bdf0f256f93732a4
Acrylic125 <acrylic125email@gmail.com>
2025-12-10 12:58:08 +0800

    feat: mobile view collapse

--- fstars/src/app/preview/page.tsx
+++ fstars/src/app/preview/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { MainNavbar } from "@/components/nav/main-navbar";
 import { TimetableModal } from "@/components/timetable/timetable-modal";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
@@ -6,32 +7,67 @@ import {
   TimetableCoursesSharedPanel,
   TimetableSharedView,
 } from "@/components/timetable/preview-timetable";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
 
-export default async function Home(props: { params: Promise<{}> }) {
+// export default async function Home(props: { params: Promise<{}> }) {
+export default function Home(props: { params: Promise<{}> }) {
+  const [sidebarClosed, setSidebarClosed] = useState(false);
   return (
     <main className="flex flex-col w-full">
       <MainNavbar />
       <div className="flex flex-col items-center">
         <div className="w-full flex flex-col lg:flex-row max-w-ui h-[calc(100svh-3.5rem)] md:h-[calc(100svh-4rem)] lg:h-fit">
-          <ScrollArea className="relative w-full flex flex-col h-1/2 lg:h-[calc(100svh-4rem)] overflow-x-auto">
+          <ScrollArea
+            className={cn("relative w-full flex flex-col overflow-x-auto", {
+              "h-1/2 lg:h-[calc(100svh-4rem)]": !sidebarClosed,
+              "h-full lg:h-[calc(100svh-4rem)]": sidebarClosed,
+            })}
+          >
             <div className="pl-4 pr-2 md:pl-8 md:pr-4 py-1 text-xs md:text-sm bg-sky-100 dark:bg-sky-800">
               You are on a shared timetable. To use it, import it.
             </div>
             <div className="w-full flex flex-col min-w-5xl pl-4 pr-2 md:pl-8 md:pr-4 py-8 gap-4">
-              <Suspense>
-                <TimetableSharedView />
-              </Suspense>
+              <TimetableSharedView />
               <div className="w-full h-20 md:h-24 lg:h-28" />
             </div>
             <ScrollBar orientation="horizontal" />
             <TimetableViewWeekSelector className="absolute bottom-4 md:bottom-8 lg:bottom-12 left-1/2 -translate-x-1/2" />
+            <div className="absolute top-0 right-0 hidden lg:flex z-10">
+              <Button
+                variant="secondary"
+                onClick={() => setSidebarClosed(!sidebarClosed)}
+              >
+                {sidebarClosed ? <ChevronLeft /> : <ChevronRight />}
+              </Button>
+            </div>
+            <div className="absolute bottom-4 md:bottom-8 lg:bottom-12 right-8 lg:hidden z-10">
+              <Button
+                variant="secondary"
+                onClick={() => setSidebarClosed(!sidebarClosed)}
+              >
+                {sidebarClosed ? <ChevronUp /> : <ChevronDown />}
+              </Button>
+            </div>
           </ScrollArea>
-          <ScrollArea className="w-full lg:w-xl relative group flex flex-col h-1/2 lg:h-[calc(100svh-4rem)] border-t border-border lg:border-0">
-            <div className="flex flex-col gap-2 md:gap-4 items-center p-2 lg:py-8 lg:pl-4 lg:pr-8">
-              <Suspense>
-                <TimetableCoursesSharedPanel />
-              </Suspense>
+          <ScrollArea
+            className={cn(
+              "w-full relative group flex flex-col border-t border-border lg:border-0",
+              {
+                "h-1/2 lg:h-[calc(100svh-4rem)] lg:w-xl": !sidebarClosed,
+                "h-0 lg:h-[calc(100svh-4rem)] lg:w-0": sidebarClosed,
+              }
+            )}
+          >
+            <div className="flex flex-col gap-2 md:gap-4 items-center p-2 pb-32 lg:py-8 lg:pl-4 lg:pr-8">
+              <TimetableCoursesSharedPanel />
             </div>
           </ScrollArea>
         </div>
--- fstars/src/app/timetable/[id]/page.tsx
+++ fstars/src/app/timetable/[id]/page.tsx
@@ -1,8 +1,6 @@
+"use client";
 import { MainNavbar } from "@/components/nav/main-navbar";
-import {
-  TimetableSelfView,
-  TimetableView,
-} from "@/components/timetable/timetable-view";
+import { TimetableSelfView } from "@/components/timetable/timetable-view";
 import {
   TimetableCoursesPanel,
   TimetableHeader,
@@ -11,15 +9,32 @@ import { TimetableModal } from "@/components/timetable/timetable-modal";
 import { TimetableGeneratorPanel } from "@/components/timetable/timetable-generator-panel";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { TimetableViewWeekSelector } from "@/components/timetable/timetable-view-week-selector";
+import { use, useState } from "react";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronUp,
+  ChevronRight,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
-export default async function Home(props: { params: Promise<{ id: string }> }) {
-  const { id } = await props.params;
+export default function Home(props: { params: Promise<{ id: string }> }) {
+  // export default function Home(props: { params: { id: string } }) {
+  const { id } = use(props.params);
+
+  const [isSidebarClosed, setSidebarClosed] = useState(false);
   return (
     <main className="flex flex-col w-full">
       <MainNavbar />
       <div className="flex flex-col items-center">
         <div className="w-full flex flex-col lg:flex-row max-w-ui h-[calc(100svh-3.5rem)] md:h-[calc(100svh-4rem)] lg:h-fit">
-          <ScrollArea className="relative w-full flex flex-col h-1/2 lg:h-[calc(100svh-4rem)] overflow-x-auto">
+          <ScrollArea
+            className={cn("relative w-full flex flex-col overflow-x-auto", {
+              "h-1/2 lg:h-[calc(100svh-4rem)]": !isSidebarClosed,
+              "h-full lg:h-[calc(100svh-4rem)]": isSidebarClosed,
+            })}
+          >
             {/* <div className="w-full flex flex-col h-[50rem] md:h-[64rem] lg:h-[80rem] xl:h-[96rem] min-w-5xl pl-4 pr-2 md:pl-8 md:pr-4 py-8 gap-4"> */}
             <div className="w-full flex flex-col min-w-5xl pl-4 pr-2 md:pl-8 md:pr-4 py-8 gap-4">
               <TimetableHeader id={id} />
@@ -27,10 +42,34 @@ export default async function Home(props: { params: Promise<{ id: string }> }) {
               <div className="w-full h-20 md:h-24 lg:h-28" />
             </div>
             <ScrollBar orientation="horizontal" />
+            <div className="absolute top-0 right-0 hidden lg:flex z-10">
+              <Button
+                variant="secondary"
+                onClick={() => setSidebarClosed(!isSidebarClosed)}
+              >
+                {isSidebarClosed ? <ChevronLeft /> : <ChevronRight />}
+              </Button>
+            </div>
+            <div className="absolute bottom-4 md:bottom-8 lg:bottom-12 right-8 lg:hidden z-10">
+              <Button
+                variant="secondary"
+                onClick={() => setSidebarClosed(!isSidebarClosed)}
+              >
+                {isSidebarClosed ? <ChevronUp /> : <ChevronDown />}
+              </Button>
+            </div>
             <TimetableViewWeekSelector className="absolute bottom-4 md:bottom-8 lg:bottom-12 left-1/2 -translate-x-1/2" />
           </ScrollArea>
-          <ScrollArea className="w-full lg:w-xl relative group flex flex-col h-1/2 lg:h-[calc(100svh-4rem)] border-t border-border lg:border-0">
-            <div className="flex flex-col gap-2 md:gap-4 items-center p-2 lg:py-8 lg:pl-4 lg:pr-8">
+          <ScrollArea
+            className={cn(
+              "w-full lg:w-xl relative group flex flex-col border-t border-border lg:border-0",
+              {
+                "h-1/2 lg:h-[calc(100svh-4rem)] lg:w-xl": !isSidebarClosed,
+                "h-0 lg:h-[calc(100svh-4rem)] lg:w-0": isSidebarClosed,
+              }
+            )}
+          >
+            <div className="flex flex-col gap-2 md:gap-4 items-center p-2 pb-32 lg:py-8 lg:pl-4 lg:pr-8">
               <TimetableCoursesPanel id={id} />
               <TimetableGeneratorPanel timetableId={id} />
             </div>
